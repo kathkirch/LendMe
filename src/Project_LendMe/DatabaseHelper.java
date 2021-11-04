9f81fe4748a47e606adcb7e7c759307f34433906
Katharina <katharina.kirchsteiger@edu.fh-joanneum.at>
2021-11-04 11:08:18 +0100

    create Project, add DB connection

---
+++ src/Project_LendMe/DatabaseHelper.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Project_LendMe;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Katharina
+ */
+public class DatabaseHelper {
+    
+    private static Connection con;
+    
+    public static void connectDB(){
+        con = null;
+        
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("JDBC Driver loaded");
+            con = DriverManager.getConnection("jdbc:mysql://10.25.2.145:3306/hermanna19?user=hermanna19&password=geb19&serverTimezone=CET");
+            System.out.println("Connection to the DB successfully established");
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.out.println(ex);
+            System.out.println("Connection to DB failed");
+            System.exit(1);
+        }
+    }
+    
+}
